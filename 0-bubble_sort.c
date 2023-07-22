753580b0a28f15b234d66f1d5e6c9797e999943d
Kook16 <jamesss075@gmail.com>
2023-07-22 14:27:50 +0000

    optimized the code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,10 +8,10 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t pass, j, swap;
-	int temp;
+	size_t pass, j;
+	int temp, swap;
 
-	for (pass = size; pass > 0 && swap; pass--)
+	for (pass = size - 1; pass >= 1 && swap; pass--)
 	{
 		swap = 0;
 		for (j = 0; j <= pass - 1; j++)
@@ -21,7 +21,7 @@ void bubble_sort(int *array, size_t size)
 				swap = 1;
 				temp = array[j];
 				array[j] = array[j + 1];
-				array[j] = temp;
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
